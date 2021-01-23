cde0dd7d635eeaed9f862b36e2059e0131cdb94f
Your Name <you@example.com>
2021-01-23 13:58:06 +0300

    [занятие 2] Препроцессоры

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,43 @@
 const { src, dest, series, watch } = require('gulp');
+const sass = require('gulp-sass');
 const cl = require('gulp-clean');
+const autoprefixer = require('gulp-autoprefixer');
+// const px2rem = require('gulp-smile-px2rem');
+var pxtorem = require('gulp-pxtorem');
+const sourcemaps = require('gulp-sourcemaps');
+const sassGlob = require('gulp-sass-glob');
+const gcmq = require('gulp-group-css-media-queries');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
+
+sass.compiler = require('node-sass');
 
 /* Копируем все содержимое из папки src в dist */
 const copy = () => {
-    return src('src/**/*.*')
+    return src('src/**/*.html')
         .pipe(dest('dist'))
+}
+
+const compileScss = () => {
+    return src(styles)
+        .pipe(concat('main.scss'))
+        .pipe(sassGlob())
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass().on('error', sass.logError)
+        )
+        // .pipe(px2rem())
+        .pipe(pxtorem())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: true
+        }))
+        .pipe(gcmq())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write())
+        .pipe(dest('./dist/styles'))
+        .pipe(browserSync.stream());
 }
 
 /**
@@ -14,6 +46,11 @@ const copy = () => {
 const clean = () => {
     return src('dist/**/*').pipe(cl());
 }
+
+const styles = [
+    'node_modules/normalize.css/normalize.css',
+    'src/styles/main.scss'
+]
 
 /**
  * Запускаем наш сервер
@@ -29,7 +66,8 @@ const server = (done) => {
 
 const watchers = (done) => {
     watch('src/**/*.html').on('all',series(copy, browserSync.reload));
+    watch('src/**/*.scss', series(compileScss));
     done();
 }
 
-exports.default = series(clean, copy, watchers, server)
+exports.default = series(clean, compileScss, copy, watchers, server)
